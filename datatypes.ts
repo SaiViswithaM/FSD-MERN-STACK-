9eefc7f1c2586c4d0ceaf8c00faaf7aed3e2caab
SaiViswithaM <saiviswitham@gmail.com>
2026-07-18 09:09:37 +0530

    Add files via upload

---
+++ datatypes.ts
@@ -0,0 +1,27 @@
+let x:number=1;
+let y:number=2;
+let z:number=3;
+//string
+let name:String="Sai ";
+let branch:String="ai";
+let year:String="third";
+//boolean
+let ispresent:boolean=true;
+let isabsent:boolean=false;
+let isconsious:boolean=true;
+//any
+let a: any=10;
+let num: any=4584;
+let char:any=345;
+//unknown
+let value1: unknown = "Sai";
+let value2: unknown = 100;
+let value3: unknown = false;
+//undefined
+let A: undefined = undefined;
+let B: undefined = undefined;
+let C: undefined = undefined;
+//null
+let v1: null = null;
+let v2: null = null;
+let v3: null = null;
